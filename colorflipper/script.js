4832ddbbb8ad740da90a545c197702bdd57a7859
Monikamuruges <163965625+Monikamuruges@users.noreply.github.com>
2025-04-19 19:31:26 +0530

    Add files via upload

---
+++ colorflipper/script.js
@@ -0,0 +1,14 @@
+const colors=["rgb(233, 165, 177)","red","green","blue","purple","tomato"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click" , function() {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+ function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+ }
+
+ 
